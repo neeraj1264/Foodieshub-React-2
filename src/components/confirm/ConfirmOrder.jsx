1b205a62088ac1284488f800c7674df40c438a04
Neeraj Manchanda <manchandaneeraj396@gmail.com>
2024-02-25 13:30:50 +0530

    .

--- src/components/confirm/ConfirmOrder.jsx
+++ src/components/confirm/ConfirmOrder.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useCart } from "../../ContextApi";
-import  './confirm.css'
+// import  './confirm.css'
 const ConfirmOrder = () => {
 
   const { cartItems, selectedAddress } = useCart();
