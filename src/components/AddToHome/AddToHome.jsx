7aa421088bea5471e0985922f0a8279c9a21a229
Neeraj Manchanda <manchandaneeraj396@gmail.com>
2024-04-07 00:07:55 +0530

    install popup frontend change

--- src/components/AddToHome/AddToHome.jsx
+++ src/components/AddToHome/AddToHome.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import "./AddToHome.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 
 const AddToHomeModal = ({ installPrompt, onInstallClick, onCloseClick }) => {
   return (
     installPrompt && (
       <div className="install-popup-overlay">
         <div className="install-popup">
+        <div className="x" onClick={onCloseClick}>
+        <FontAwesomeIcon className="circle" icon={faCircleXmark} />
+                </div>
           <div className="installApp">
-          <img src="img/logo1.png" alt="logo"  className="install-img"/>
-            <p style={{ color: "white" }}> Install App in your device</p>
-            <p style={{ margin: "0 1rem", color: "white" }}>|</p>
+          <img src="img/logo.png" alt="logo"  className="install-img"/>
+            <p className="install-lable"> Install App in your device</p>
+            {/* <p style={{ margin: "0 1rem", color: "white" }}>|</p> */}
             <span onClick={onInstallClick} className="install">
               Install
             </span>
